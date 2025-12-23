cf4b395ee2de4302e805833873061b70321d54a4
raghadhassan812 <raghadhassan812@gmail.com>
2025-12-23 23:33:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,18 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = os.getenv("8459906106:AAGMQJQ3o1Qy-_RQZ0tqF8bMEmTSxxYN_Uw")
+TOKEN = os.environ.get("8459906106:AAGMQJQ3o1Qy-_RQZ0tqF8bMEmTSxxYN_Uw")
 
 async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ وصلتني رسالتك")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-app.run_polling()
+def main():
+    if not TOKEN:
+        raise ValueError("BOT_TOKEN not found in environment variables")
+
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
