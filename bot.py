2541a9d30a5d53fab5cfd38d27dfeaaa7d325c56
raghadhassan812 <raghadhassan812@gmail.com>
2025-12-23 18:38:58 +0300

    Create bot.py
    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+
+TOKEN = "8459906106:AAGMQJQ3o1Qy-_RQZ0tqF8bMEmTSxxYN_Uw"
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("✅ وصلتني رسالتك")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
+app.run_polling()
